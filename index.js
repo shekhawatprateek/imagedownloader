fe2147bf3773b4adc84444e8f582aead0b53fa4d
shekhawatprateek <princeshekhawat78@gmail.com>
2025-02-25 16:33:14 +0530

    images saved in fs

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-const  {images} = require("./constant.js")
+const { images } = require("./constant.js")
 const express = require("express")
+const Axios = require('axios')
 const app = express()
+const fs = require('fs')
 
-function DownloadImage(){
-    
+function saveImage(data, i) {
+    try {
+        fs.writeFileSync(`image${i}.jpg`, data, (err, res) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(res);
+
+        })
+    }
+    catch (e) {
+        console.log("Error in saveImage ", e)
+    }
 }
+
+async function DownloadImage() {
+
+    try {
+
+        for (let i = 0; i < images.length; i++) {
+
+            const response = await fetch(images[i]);
+
+            // console.log(response)
+
+            const imagesArr = await response.arrayBuffer();
+
+            // console.log(imagesArr)
+            const buffer = Buffer.from(imagesArr, 'binary');
+
+            console.log(buffer)
+            saveImage(buffer, i)
+
+        }
+
+
+    }
+    catch (error) {
+        console.log("Error in DownloadImage", error)
+    }
+}
+
+DownloadImage()
+
+
 
 
 
